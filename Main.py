09c7499083435d79ec2c27faf046399b13bde6f0
unknown <khanglam34@gmail.com>
2018-09-09 02:39:49 -0400

    Beta_weight method and get_stats class were created

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
 from Options import *
+from pandas import *
+from datetime import *
 import matplotlib.pyplot as plt
 
 def plot(Position):
@@ -24,14 +26,76 @@ def plot(Position):
     plt.xlabel('Price')
     plt.ylabel('Expire Value')
 
-def test():
-    AMD1 = Option('AMD', 'na', 'na', 24, 24.5, 0.41)
-    AMD2 = Option('AMD', 26, 27, 28, 29, 0.78)
-    APPL = Option('AMD', 26, 28, 28, 29, 1.26)
-    plot('AMD')
+class get_stats(object):
+    def __init__(self, name, days=90):
+        stock = Stock(name)
+        stock.get_open()
+
+        start = datetime.now() + timedelta(days = days*-1 )
+        end = datetime.now()
+
+        data = get_historical_data(name, start=start, end=end, output_format = 'pandas')
+        diff = []
+        for n in range(1, len(data['close'])):
+            result = ((data['close'][n]/data['close'][n-1])-1)*100
+            diff.append(result)
+        hold = DataFrame({'Daily Return': diff})
+        dailyReturn = hold['Daily Return']
+
+        n = len(dailyReturn)
+        mean = sum(dailyReturn)/n
+        standardDeviation = (sum((dailyReturn - mean)**2)/n)**.5
+
+        self.name = name
+        self.currentPrice = stock.get_price()
+        self.SD = standardDeviation
+        self.DR = dailyReturn
+        self.size = n
+        self.mean = mean
+
+    def get_price(self):
+        return self.currentPrice
+
+def Get_Beta(sample, index , days = 90):
+    cov = sum((sample.DR - sample.mean)*(index.DR - index.mean))/(index.size-1)
+    return cov/index.SD
+
+def Beta_weight(OptionDict):
+    INDEX = get_stats('SPY')
+    SPYPrice = INDEX.get_price()
+    ref = .92
+    xVal = []
+    yVal = []
+    while ref < 1.08:
+        price = SPYPrice * ref
+        xVal.append(price)
+        portfolioValue = 0
+        for key in Option.List.keys():
+            stock = get_stats(key)
+            for position in Option.List[key]:
+                stockprice = (Get_Beta(stock,INDEX)*(price/SPYPrice - 1)+1)*stock.currentPrice
+                print(stockprice, price)
+                currentValue = position.getValue(stockprice)
+                portfolioValue += currentValue
+        yVal.append(portfolioValue)
+        ref += 0.005
+    plt.figure('Beta Weight')
+    plt.plot(xVal, yVal)
+    plt.axvline(x=SPYPrice, color='r', linestyle='--')
+    plt.axhline(y=0, color='g', linestyle='-')
+
+    plt.title('Beta Weight')
+    plt.grid()
+    plt.xlabel('Price')
+    plt.ylabel('Expire Value')
     plt.show()
 
+def Main():
+    AMD = Option('AMD', 'na', 'na', 30, 35, .87, 10)
+    FXI = Option('FXI',34, 41, 41, 47, 2.38, 10)
+
+    Beta_weight(Option.List)
 
 
 
-test()
+Main()
--- Options.py
+++ Options.py
@@ -13,8 +13,7 @@ contract = # of option contract
 '''
 
 from iexfinance import *
-from datetime import *
-from pandas import *
+
 class Option(object):
     List = {}
     def __init__(self, name, LPsp, SPsp, SCsp, LCsp, OP, contract =1):
@@ -28,6 +27,8 @@ class Option(object):
         self.LCsp = LCsp
         self.OP = OP
         self.contract = contract
+        # Everytime a new Option instance is created it will check to see if the instance name has already been used, if so
+        # it will add that instance to the Option.List dict
         if name in Option.List:
             Option.List[name].append(self)
         else:
@@ -84,68 +85,3 @@ class Option(object):
 
     def __str__(self):
         return '[' +self.name+' ' +str(self.LPsp)+'/'+str(self.SPsp)+'/'+str(self.SCsp)+'/'+str(self.LCsp)+ ' @'+str(self.OP)+']'
-
-    def get_diff(self, days):
-        startDate = datetime.now() + timedelta(days = days*-1 )
-        endDate = datetime.now()
-        data = get_historical_data(self.name, start=startDate, end=endDate, output_format = 'pandas')
-        diff = []
-        for n in range(1, len(data['close'])):
-            result = ((data['close'][n]/data['close'][n-1])-1)*100
-            diff.append(result)
-
-        Return = DataFrame({'Daily return':[.9,1.3,1.7,0.4,0.7]})
-        return Return['Daily return']
-
-    def SPY_diff(self, days):
-        startDate = datetime.now() + timedelta(days = days*-1 )
-        endDate = datetime.now()
-        data = get_historical_data('SPY', start=startDate, end=endDate, output_format = 'pandas')
-        diff = []
-        for n in range(1, len(data['close'])):
-            result = ((data['close'][n] / data['close'][n - 1]) - 1) * 100
-            diff.append(result)
-
-        Return = DataFrame({'Daily return':[2.5,3.5,3.6,3.1,2.3]})
-        return Return['Daily return']
-
-    def correlation(self, days =365):
-        stockReturn = self.get_diff(days)
-        SPYReturn = self.SPY_diff(days)
-        print(stockReturn, SPYReturn)
-
-        n = len(stockReturn)
-        print(n)
-        stockMean = sum(stockReturn)/n
-        SPYMean = sum(SPYReturn)/n
-        print(stockMean,SPYMean)
-
-        cov = sum((stockReturn-stockMean)*(SPYReturn-SPYMean))/(n-1)
-        print(cov)
-        stockVar = sum((stockReturn - stockMean)**2)/(n-1)
-        SPYVar = sum((SPYReturn - SPYMean)**2)/(n-1)
-        print(stockReturn)
-        print(sum((stockReturn-stockMean)**2))
-        print(stockVar,SPYVar)
-
-        stockSD = stockVar**(0.5)
-        SPYSD = SPYVar**(0.5)
-        print(stockSD,SPYSD)
-        Corr = cov/(stockSD - SPYSD)
-
-        return Corr
-
-def test():
-    AMD = Option('AAPL', 26, 28, 28, 29, 1.26)
-    print(AMD.correlation())
-
-test()
-
-
-
-
-
-
-
-
-
